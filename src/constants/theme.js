6d940d7b332a120fbeb0836bfaec36bf60424c8c
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-09-07 10:46:24 -0300

    Correções no Router

---
+++ src/constants/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#5CB646",
+      contrastText: "#fff",
+    },
+    text: {
+      primary: "#000",
+    },
+  },
+});
+
+export default theme;
