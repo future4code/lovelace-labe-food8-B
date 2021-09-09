4bfeb19da8b601f78b7923fe885e9031097b5e19
Lucas Fernandes <lucas_matheus22@hotmail.com>
2021-09-09 16:21:00 -0300

    Estilização da Home já integrada com API e criação do componente para renderizar a lista de restaurantes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,157 +1,66 @@
-import React, { useEffect, useState } from 'react';
-import { InputAdornment, TextField } from '@material-ui/core'
-import SearchIcon from '@material-ui/icons/Search';
-import styled from "styled-components";
-import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
-import axios from "axios"
-
-
-// const MainContainer = styled.div`
-//     width: 100vw;
-//     max-width: 420px;
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-//     align-items: center;
-//     margin-bottom: 70px;  
-// `
-
-const RestaurantCard = styled.div`
-display: flex;  
-width: 360px;
-  height: 196px;
-  margin: 50px 0 0;
-  padding: 8px 16px 0;
-`
-const Rectangle = styled.div`
-width: 328px;
-  height: 188px;
-  padding: 0 0 16px;
-  border-radius: 8px;
-  border: solid 1px #b8b8b8;
-  img {
-    width: 328px;
-    height: 120px;
-    margin: 0 0 12px;
-    object-fit: contain;
-  }
-`
-const Restaurant = styled.div`
-  width: 296px;
-  height: 18px;
-  margin: 12px 16px 4px;
-  font-family: Roboto;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: -0.39px;
-  color: #5cb646;
-`
-
-const DeliveryTime = styled.div`
-Tempo-de-entrega {
-  width: 148px;
-  height: 18px;
-  margin: 12px 16px 4px;
-  padding-left: 30px;
-  font-family: Roboto;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: -0.39px;
-  color: #b8b8b8;
-`
-
-const Shipping = styled.div`
-  width: 140px;
-  height: 18px;
-  margin: 4px 16px 0 8px;
-  font-family: Roboto;
-  font-size: 16px;
-  font-weight: normal;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: -0.39px;
-  text-align: right;
-  color: #b8b8b8;
-`
+import React, { useEffect, useState } from "react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/input";
+import { Box, Container, Divider, Heading } from "@chakra-ui/layout";
+import { BiSearch } from "react-icons/bi";
+import axios from "axios";
+import { appName, baseUrl } from "../constants/constants";
+import RestaurantCard from "../components/RestaurantCard";
 
 const HomePage = () => {
-
-  const [restaurants, setRestaurants] = useState([])
+  const [restaurantsList, setRestaurantsList] = useState([]);
 
   useEffect(() => {
-    getRestaurants()
-  }, [])
+    getRestaurants();
+  }, []);
 
   const getRestaurants = () => {
-    // https://us-central1-missao-newton.cloudfunctions.net/{{appName}}/restaurants
-    axios.get('https://us-central1-missao-newton.cloudfunctions.net/futureEatsE/restaurants', {
+    const axiosConfig = {
       headers: {
-        auth: localStorage.getItem("token")
-      }
-    }).then((res) => {
-      console.log(res.data.restaurants)
-      setRestaurants(res.data.restaurants)
+        auth: localStorage.getItem("token"),
+      },
+    };
 
-    })
-      .catch(err => { console.log(err.message) })
-  }
-
-  // { !restaurants && "Carregando..." }
-
-  const renderRestaurants =
-    restaurants.length !== 0 && restaurants.map((item) => {
-      return (
-        <RestaurantCard>
-          <Rectangle>
-            <img src={item.logoUrl} />
-            <Restaurant>
-            <p> {item.name}</p>
-            </Restaurant>
-            <DeliveryTime>
-            <p> {item.deliveryTime} min</p>
-            </DeliveryTime>
-            <Shipping>
-              {item.shipping}
-            </Shipping>
-          </Rectangle>
-        </RestaurantCard>
-
-      )
-    })
-
+    axios
+      .get(`${baseUrl}/${appName}/restaurants`, axiosConfig)
+      .then((res) => {
+        console.log(res.data.restaurants);
+        setRestaurantsList(res.data.restaurants);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
-    <div>
-      <TextField
-        variant="outlined"
-        size="medium"
-        // value={searchTerm}
-        type="text"
-        name="restaurant"
-        placeholder="Procure um restaurante"
-        // onChange={handleChange}
-        required
-        style={{ margin: '8px 0 0 0', width: '100%' }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          )
-        }}
-      />
+    <Container h="100vh">
+      {/* Topo do App */}
+      <Heading as="h1" fontSize="lg" textAlign="center" p="4">
+        FutureEats
+      </Heading>
+      <Divider mb="2" />
 
-      {renderRestaurants}
+      {/* Input pesquisa */}
+      <InputGroup mb="4">
+        <InputLeftElement
+          pointerEvents=""
+          children={<BiSearch size="24px" color="gray.300" />}
+          mt="1"
+          mx="1"
+        />
+        <Input mx="1" size="lg" />
+      </InputGroup>
 
-    </div>
-  )
+      {/* Menu lateral por categoria */}
+      <Box w="full" textAlign="center" background="gray.300" p="2">
+        MENU LATERAL
+      </Box>
+
+      {/* Lista de restaurantes com componente separado */}
+      {restaurantsList.map((restaurant) => (
+        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+      ))}
+    </Container>
+  );
 };
 
 export default HomePage;
