050689f9c3fb49a74bcea6cd649ec80968bb3987
unknown <claricepassos@gmail.com>
2021-09-10 18:46:01 -0300

    estado global

--- src/global/GlobalState.js
+++ src/global/GlobalState.js
@@ -5,48 +5,55 @@ const GlobalState = (props) => {
     const [cart, setCart] = useState([]);
 
     const addToCart = (product) => {
-        const isProductAlreadyInCart = cart.find((productInCart) => {
-            if (productInCart.id === product.id) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-
-        if (isProductAlreadyInCart) {
-            const newCart = cart.map((productInCart) => {
-                if (productInCart.id === product.id) {
-                    const newProduct = {
-                        ...productInCart,
-                        quantity: productInCart.quantity + 1
-                    };
-                    return newProduct;
-                }
-                return productInCart;
-            });
-            setCart(newCart);
+      const isProductAlreadyInCart = cart.find((productInCart) => {
+        if (productInCart.id === product.id) {
+          return true;
         } else {
-            const newProduct = { ...product, quantity: 1 };
-            const newCart = [...cart, newProduct];
-            setCart(newCart);
+          return false;
         }
-    };
-
-    const removeFromCart = (product) => {
-        let newCart = cart.map((productInCart) => {
-            if (productInCart.id === product.id) {
-                const newProduct = {
-                    ...productInCart,
-                    quantity: productInCart.quantity - 1
-                };
-                return newProduct;
-            }
-            return productInCart;
+      });
+  
+      if (isProductAlreadyInCart) {
+        const newCart = cart.map((productInCart) => {
+          if (productInCart.id === product.id) {
+            const newProduct = {
+              ...productInCart,
+              quantity: productInCart.quantity + 1
+            };
+            return newProduct;
+          }
+          return productInCart;
         });
-
-    const state = {cart}
-    const setters = {addToCart, removeFromCart}
-    const requests = {}
+        setCart(newCart);
+      } else {
+        const newProduct = { ...product, quantity: 1 };
+        const newCart = [...cart, newProduct];
+        setCart(newCart);
+      }
+    };
+  
+    const removeFromCart = (product) => {
+      let newCart = cart.map((productInCart) => {
+        if (productInCart.id === product.id) {
+          const newProduct = {
+            ...productInCart,
+            quantity: productInCart.quantity - 1
+          };
+          return newProduct;
+        }
+        return productInCart;
+      });
+  
+      newCart = newCart.filter((productInCart) => {
+        if (productInCart.quantity < 1) {
+          return false;
+        } else {
+          return true;
+        }
+      });
+  
+      setCart(newCart);
+    };
 
         return (
             <GlobalContext.Provider value={{ state, setters, requests }}>
@@ -54,5 +61,5 @@ const GlobalState = (props) => {
             </GlobalContext.Provider>
         );
     };
-}
+
 export default GlobalState;
